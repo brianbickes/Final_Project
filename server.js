aadd9460307b4ad8cc108827bc6b9f276dfcf363
brianbickes <brianbickes@gmail.com>
2020-09-26 12:43:26 -0700

    Added models, views, controllers and public folder. Added boilerplate for server.js file

---
+++ server.js
@@ -0,0 +1,46 @@
+require('dotenv').config();
+
+// DEPENDENCIES
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const methodOverride = require('method-override');
+const PORT = process.env.PORT || 3000;
+
+// MIDDLEWARE
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: false }));
+app.set('view engine', 'jsx');
+app.engine('jsx', require('express-react-views').createEngine());
+app.use(methodOverride('_method'));
+const mongoURI = process.env.MONGO_URI;
+
+// MONGOOSE CONNECTION
+mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connection.once('open', () => {
+    console.log("connected to mongo");
+});
+
+//CONTROLLERS
+
+//CLASSES
+const dogsController = require('./controllers/dogs.js');
+app.use('/dogs', dogsController); 
+
+//STATIC PAGES
+
+app.get('/', (req, res) => {
+    res.render('static/Home')
+});
+
+app.get('/about', (req, res) => {
+    res.render('static/About') 
+})
+app.get('/contact-us', (req, res) => {
+    res.render('static/ContactUs')
+});
+
+//LISTENER
+app.listen(PORT, ()=>{
+    console.log("I am always listening....")
+}); 
